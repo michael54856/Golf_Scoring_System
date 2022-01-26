6a2403ec7f2ec8ddac7bb1d0d711a7e4fd305099
michael548562 <michael548562@gmail.com>
2022-01-26 13:34:39 +0800

    update

--- realTime/realTimeDetect.py
+++ realTime/realTimeDetect.py
@@ -6,10 +6,6 @@ import math
 import time
 import glob
 
-#subprocess.Popen(["C:\Users\michael\Desktop\openpose\build\x64\Debug\OpenPoseDemo.exe","--model_folder","C:\Users\michael\Desktop\openpose\models","--num_gpu_start","0","--process_real_time","--display","0","-number_people_max","1","--net_resolution","640x480","--write_json","MyKey2,"--render_pose","0"])
-
-
-
 
 def generateVector(fileName): #生成這個frame的向量資訊(讀取json)
     myVector = []
@@ -205,28 +201,22 @@ def angleDiff(vector_1,vector_2): #回傳兩個向量的角度差
     return angle*57.3
  
 
-def mergeFrame(number,fileName_1,fileName_2) :
+def mergeFrame(number,VectorFrame_1,VectorFrame_2) :
     file_2_percent = number-math.floor(number)
     file_1_percent = 1-file_2_percent
-    mergeList = [None] * 75
-    with open(fileName_1) as f:#開啟第一個檔案
-        data = json.load(f)
-        keys = data["people"][0]["pose_keypoints_2d"]
-        i = 0
-        while i < 75 :
-            mergeList[i] = keys[i]*file_1_percent
-            i += 1
+    mergeList = []
+    i = 0
+    while i < 16 :
+        mergeList.append([VectorFrame_1[i][0]*file_1_percent,VectorFrame_1[i][1]*file_1_percent])
+        i += 1
 
-    with open(fileName_2) as f:#開啟第二個檔案
-        data = json.load(f)
-        keys = data["people"][0]["pose_keypoints_2d"]
-        i = 0
-        while i < 75 :
-            if mergeList[i] == 0 :#如果上一個frame沒偵測到,那就直接使用這個frame的資訊
-                mergeList[i] = keys[i]
-            else:
-                mergeList[i] += keys[i]*file_2_percent
-            i += 1
+    while i < 16 :
+        if mergeList[i][0] == 0 and  mergeList[i][1] == 0:#如果上一個frame沒偵測到,那就直接使用這個frame的資訊
+            mergeList[i] = [VectorFrame_2[i][0],VectorFrame_2[i][1]]
+        else:
+            mergeList[i][0] += VectorFrame_2[i][0]*file_2_percent
+            mergeList[i][1] += VectorFrame_2[i][1]*file_2_percent
+        i += 1
 
     return mergeList
 
@@ -240,20 +230,51 @@ def VectorDiffLoss(vec1,vec2):
             points += 1 #加一意思一下
         else :
             t = angleDiff(vec1[i],vec2[i])
-            t = (t/5)**(2) #相差5度內沒什麼關係
+            t = (t/6.25)**(2) #相差5度內沒什麼關係
             points += t
         i+=1
-    if zeroVectorCount >= 5:
+    if zeroVectorCount >= 8: #如果超過5個向量沒被偵測,讓分數一次加很多
         points += 5000
     return points
+
+def scoreCalculate(VectorList,path_to_Target):
+    Target_json_files = [pos_json for pos_json in os.listdir(path_to_Target) if pos_json.endswith('.json')]
+    totalLoss = 0
+    for index, currentFrameName in enumerate(Target_json_files):
+        currentVector = generateVector(path_to_Target+currentFrameName) #取得target的向量資訊
+
+        percent = (index+1) / len(Target_json_files) #取得這個frame所占百分比
+        whichToChoose =  percent*len(VectorList)
+
+        compareVector = []
+        if whichToChoose < 1 : #直接使用第一個
+            compareVector = VectorList[0] #取得要比對的向量資訊
+        else:
+            if whichToChoose % 1 != 0: #如果是小數,要用2個frame做merge
+                compareVector = mergeFrame(whichToChoose,VectorList[math.floor(whichToChoose)-1],VectorList[math.floor(whichToChoose)])
+            else: #整數直接取用
+                compareVector = VectorList[int(whichToChoose)-1] #取得要比對的向量資訊
+        totalLoss += VectorDiffLoss(currentVector,compareVector)
+    totalLoss /= len(Target_json_files)
+    return totalLoss
+
 
 lastFiles = glob.glob('Mykey/*')
 for f in lastFiles:
     os.remove(f)
 subprocess.Popen(["C:\\Users\\michael\\Desktop\\openpose\\build\\x64\\Debug\\OpenPoseDemo.exe","--model_folder","C:\\Users\\michael\\Desktop\\openpose\\models","--num_gpu_start","0","--process_real_time","-number_people_max","1","--net_resolution","640x480","--write_json","MyKey"])
 fileNumber = 0
-swingCount = 0
-detectReady = False
+
+swingState = 0
+#0->沒有準備
+#1->已經準備完成,將要預備揮桿
+#2->揮桿預備完成,將要揮桿
+
+ReadyVectorList = []
+SwingVectorList = []
+
+score = 0
+
 while True:
     nextFileNumber = fileNumber+1
     numberDigits_1 = len(str(fileNumber))
@@ -276,16 +297,51 @@ while True:
         data = json.load(f)
         if len(data["people"]) > 0 :
             currentVector = generateVector(fileName) #取得target的向量資訊
-            ready_compareVector = generateVector('readyPose.json') #取得要比對的向量資訊
-            end_compareVector = generateVector('endPose.json') #取得要比對的向量資訊
-            #print(f'{VectorDiffLoss(currentVector,ready_compareVector)} {VectorDiffLoss(currentVector,end_compareVector)}')
-            if VectorDiffLoss(currentVector,ready_compareVector) < 300 :
-                detectReady = True
-            if VectorDiffLoss(currentVector,end_compareVector) < 300 :
-                if detectReady == True:
-                    detectReady = False
-                    swingCount += 1
-                    print(f'detect swing : {swingCount}')
+
+            if swingState == 1:
+                ReadyVectorList.append(currentVector)
+
+            if swingState == 2:
+                SwingVectorList.append(currentVector)
+            
+            ready_begin_compareVector = generateVector('Output_Key/Front_Ready_Begin_keypoints.json') #取得要比對的向量資訊
+            ready_end_compareVector = generateVector('Output_Key/Front_Ready_End_keypoints.json') #取得要比對的向量資訊
+            swing_end_compareVector = generateVector('Output_Key/Front_Swing_End_keypoints.json') #取得要比對的向量資訊
+
+
+            #print(f'{VectorDiffLoss(currentVector,ready_begin_compareVector)} {VectorDiffLoss(currentVector,ready_end_compareVector)} {VectorDiffLoss(currentVector,swing_end_compareVector)}')
+            
+            if swingState == 0 or swingState == 1 :
+                if VectorDiffLoss(currentVector,ready_begin_compareVector) < 300 : #重新抓取
+                    ReadyVectorList.clear()
+                    ReadyVectorList.append(currentVector)
+                    score = 0
+                    swingState = 1
+            
+            if swingState == 1 or swingState == 2:
+                if VectorDiffLoss(currentVector,ready_end_compareVector) < 575 : #偵測到預備揮桿完成
+                    if swingState == 1 :
+                        score += scoreCalculate(ReadyVectorList,'Output_Video_Key/Front_Ready/')
+                        swingState = 2
+                        print(f'detect ready pose : , ' , end='')
+                    SwingVectorList.clear()
+                    SwingVectorList.append(currentVector)
+            
+            if swingState == 2:
+                if VectorDiffLoss(currentVector,swing_end_compareVector) < 675 : #偵測到完整揮桿完成
+                    swingState = 0
+                    score += scoreCalculate(SwingVectorList,'Output_Video_Key/Front_Swing/')
+                    score /= 2
+                    score = (score-300) / 1000
+                    score = (1-score) * 100 
+                    if score > 100 :
+                        score = 100
+                    if score < 0 :
+                        score = 0
+                    print(f'your swing score : {round(score,1)}')
+            
+    
+    os.remove(fileName)
 
     fileNumber += 1
 
